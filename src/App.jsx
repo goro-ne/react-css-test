eab7f806af94942b9bb7e9fd1a96788d446e7e7d
goro-ne <hayao56@gmail.com>
2024-07-29 11:53:58 +0900

    Styled Componentsテスト

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
 import { StyledJsx } from './components/StyledJsx';
+import { StyledComponents } from './components/StyledComponents';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         <InlineStyle />
         <CssModules />
         <StyledJsx />
+        <StyledComponents />
       </div>
     </>
   );

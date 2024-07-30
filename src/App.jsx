75e7675face10176f7d517453228464449432d53
goro-ne <hayao56@gmail.com>
2024-07-30 10:29:20 +0900

    @emotion,@emotion/styled test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
 import { StyledJsx } from './components/StyledJsx';
 import { StyledComponents } from './components/StyledComponents';
+import { Emotion } from './components/Emotion';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
         <CssModules />
         <StyledJsx />
         <StyledComponents />
+        <Emotion />
       </div>
     </>
   );
